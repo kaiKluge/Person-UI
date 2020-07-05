6b9c393f903eb856112236ea4c9c6678cb9a9d2d
Kai Kluge <kluge@candy4you.de>
2020-07-05 14:28:27 +0200

    Built user interface according to requirements document for challenge.

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Kai Kluge on 04.07.20.
+//  Copyright © 2020 Kai Kluge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
